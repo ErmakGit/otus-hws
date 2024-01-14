9446075e2bc80e2a5e390235bab5c8272273fada
Anton Ermakov <a.ermakov@mercurydevelopment.com>
2024-01-14 20:05:28 +0800

    HW02: refactored code by code review notes. Added new tests.

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,11 +8,14 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+const slashStr = `\`
+
 func Unpack(input string) (string, error) {
 	var res strings.Builder
 	skipNext := false
 	skipCheck := false
-	for ind, char := range input {
+	runes := []rune(input)
+	for ind, char := range runes {
 		if skipNext {
 			skipNext = false
 			skipCheck = false
@@ -23,17 +26,23 @@ func Unpack(input string) (string, error) {
 			return "", ErrInvalidString
 		}
 
-		if ind+1 == len(input) {
+		if ind+1 == len(runes) {
+			if string(char) == slashStr {
+				return "", ErrInvalidString
+			}
 			res.WriteRune(char)
 			continue
 		}
 
-		if string(char) == `\` && !skipCheck {
+		if string(char) == slashStr && !skipCheck {
+			if _, err := strconv.Atoi(string(runes[ind+1])); err != nil && string(runes[ind+1]) != slashStr {
+				return "", ErrInvalidString
+			}
 			skipCheck = true
 			continue
 		}
 
-		if count, err := strconv.Atoi(string(input[ind+1])); err == nil {
+		if count, err := strconv.Atoi(string(runes[ind+1])); err == nil {
 			res.WriteString(strings.Repeat(string(char), count))
 			skipNext = true
 		} else {
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,8 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "aaб2b", expected: "aaббb"},
+		{input: "aaб0b", expected: "aab"},
 		// uncomment if task with asterisk completed
 		{input: `qwe\4\5`, expected: `qwe45`},
 		{input: `qwe\45`, expected: `qwe44444`},
@@ -35,7 +37,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qwe\`, `qwe\e`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
