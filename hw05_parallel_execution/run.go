2f0707e5d5f949f463e3b92531d167bcccf3f8f8
Anton Ermakov <a.ermakov@mercurydevelopment.com>
2024-04-01 16:40:57 +0800

    HW05: implemented parallel execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -8,8 +8,63 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func worker(id int, jobs <-chan Task, results chan<- bool, stop <-chan struct{}) {
+	select {
+	case <-stop:
+		return
+	default:
+	}
+
+	for job := range jobs {
+		// fmt.Printf("Goroutine %v getting task \n", id)
+		execute(job, results)
+	}
+}
+
+func execute(task Task, results chan<- bool) {
+	err := task()
+	executed := true
+	if err != nil {
+		executed = false
+	}
+
+	results <- executed
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	// Place your code here.
+
+	jobs := make(chan Task, len(tasks))
+	results := make(chan bool, len(tasks))
+	stop := make(chan struct{}, 1)
+
+	// wg:= sync.WaitGroup{}
+
+	for w := 1; w <= n; w++ {
+		// wg.Add(1)
+		go worker(w, jobs, results, stop)
+	}
+	// wg.Wait()
+
+	for _, job := range tasks {
+		jobs <- job
+	}
+	close(jobs)
+
+	errorsCounter := 0
+	for res := 1; res <= len(tasks); res++ {
+		if errorsCounter == m {
+			stop <- struct{}{}
+
+			return ErrErrorsLimitExceeded
+		}
+
+		result := <-results
+		if !result {
+			errorsCounter++
+		}
+	}
+
 	return nil
 }
