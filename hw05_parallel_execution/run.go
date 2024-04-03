8c5baa4a45ce7d5d3524abb02e4df58e8aeef77b
Anton Ermakov <a.ermakov@mercurydevelopment.com>
2024-04-03 18:47:38 +0800

    HW05: refactored code. Resolve problem with Race

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,24 +3,31 @@ package hw05parallelexecution
 import (
 	"errors"
 	"sync"
-	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-type ErrorsCounter struct {
+type ErrorCounter struct {
 	mu    sync.RWMutex
 	count int64
 }
 
-func worker(wg *sync.WaitGroup, tasks <-chan Task, errCounter *ErrorsCounter) {
+func worker(wg *sync.WaitGroup, tasks <-chan Task, errLimit int64, errCounter *ErrorCounter) {
 	for task := range tasks {
+		errCounter.mu.RLock()
+		countErr := errCounter.count
+		errCounter.mu.RUnlock()
+
+		if errLimit > 0 && countErr > errLimit {
+			break
+		}
+
 		err := task()
 		if err != nil {
 			errCounter.mu.Lock()
-			atomic.AddInt64(&errCounter.count, 1)
+			errCounter.count++
 			errCounter.mu.Unlock()
 		}
 	}
@@ -30,21 +37,23 @@ func worker(wg *sync.WaitGroup, tasks <-chan Task, errCounter *ErrorsCounter) {
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	jobs := make(chan Task)
-	var errCounter ErrorsCounter
+	var errCounter ErrorCounter
 
 	wg := sync.WaitGroup{}
 	for w := 1; w <= n; w++ {
 		wg.Add(1)
-		go worker(&wg, jobs, &errCounter)
+		go worker(&wg, jobs, int64(m), &errCounter)
 	}
 
 	for _, task := range tasks {
 		errCounter.mu.RLock()
-		errCount := errCounter.count
+		countErr := errCounter.count
 		errCounter.mu.RUnlock()
 
-		if m > 0 && int(errCount) > m {
+		if m > 0 && int(countErr) > m {
 			close(jobs)
+			wg.Wait()
+
 			return ErrErrorsLimitExceeded
 		}
 
