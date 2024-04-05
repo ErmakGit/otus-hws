3df634127c7d541d825cb11b706638adda783c90
Anton Ermakov <a.ermakov@mercurydevelopment.com>
2024-04-05 21:22:55 +0800

    HW06: implemented pipelent execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,38 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var out Out
+	for k, stage := range stages {
+		if k == 0 {
+			out = stage(executeStage(in, done))
+		} else {
+			out = stage(executeStage(out, done))
+		}
+	}
+
+	return out
+}
+
+func executeStage(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case out <- v:
+				}
+			}
+		}
+	}()
+
+	return out
 }
